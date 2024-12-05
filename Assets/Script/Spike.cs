6331c373db905fc912d1c6fe5e63aff39e743fbb
Brian <58400498+nairb725@users.noreply.github.com>
2024-12-05 23:41:21 +0100

    pop sound

--- Assets/Script/Spike.cs
+++ Assets/Script/Spike.cs
@@ -5,12 +5,14 @@ public class Spike : MonoBehaviour
     public string girafeTag = "Girafe";
     public Vector3 teleportPosition;
 
+    public AudioSource audioSource;
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag(girafeTag))
         {
             collision.gameObject.transform.position = teleportPosition;
             collision.gameObject.transform.rotation = Quaternion.Euler(0, 270, 0);
+            audioSource.Play();
         }
     }
 }
